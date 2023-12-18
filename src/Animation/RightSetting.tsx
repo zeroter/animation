a5f9cd09eaaf31c3b6d1d28c924695653bea697e
zeroter <510432304@qq.com>
2023-12-18 15:36:54 +0800

    feat: 完善右侧设置 并添加背景

--- src/Animation/RightSetting.tsx
+++ src/Animation/RightSetting.tsx
@@ -1,14 +1,78 @@
+import classNames from 'classnames'
 import styled from 'styled-components'
 
 interface RightSettingProps {
+  state: string
   setState: (state: string) => void
 }
 const RightSetting: React.FC<RightSettingProps> = (props) => {
-  return <Wrapper></Wrapper>
+  const items = [
+    { type: 'fadeIn', label: 'Fade in' },
+    { type: 'fadeLeft', label: 'Fade left' },
+    { type: 'flip', label: 'Flip' }
+  ]
+
+  return (
+    <Wrapper>
+      {items.map((item: any) => (
+        <div
+          className={classNames('item', {
+            selected: item.type === props.state
+          })}
+          key={item.type}
+          onClick={() => {
+            props.setState(item.type)
+          }}
+        >
+          {item.label}
+        </div>
+      ))}
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.div`
   position: fixed;
+  width: 100px;
+  height: 150px;
+  border-radius: 8px;
+
+  box-shadow: 0 0 10px #aaa;
+  right: 10px;
+  bottom: 10px;
+  background-color: #fff;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 8px;
+
+  .item {
+    width: 80px;
+    height: 32px;
+    border-radius: 4px;
+    border: 1px solid #ccc;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    user-select: none;
+    transition: all 0.3s;
+    color: #444;
+    font-size: 13px;
+
+    &:hover {
+      border-color: #409eff;
+      color: #409eff;
+    }
+
+    &.selected {
+      border-color: #409eff;
+      color: #fff;
+      background-color: #409eff;
+    }
+  }
 `
 
 export default RightSetting
--- src/Animation/index.tsx
+++ src/Animation/index.tsx
@@ -1,20 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import styled from 'styled-components'
 import RightSetting from './RightSetting'
 
 const Animation = () => {
-  const [state, setState] = useState<string>('fade')
+  const [state, setState] = useState<string>('fadeIn')
+  const [isSmall, setIsSmall] = useState<boolean>(false)
+
+  const bgImage = useMemo(() => {
+    return isSmall ? `./public/mobile_bg.jpg` : './public/pc_bg.png'
+  }, [isSmall])
+
+  useEffect(() => {
+    function updateClient() {
+      setIsSmall(window.innerWidth < 576)
+    }
+
+    window.addEventListener('resize', updateClient)
+  }, [])
 
   return (
-    <Wrapper>
-      <RightSetting setState={setState}></RightSetting>
+    <Wrapper style={{ backgroundImage: bgImage }}>
+      <img src={bgImage} />
+      <RightSetting setState={setState} state={state}></RightSetting>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  min-height: 100vh;
+
+  img {
+    width: 100%;
+    overflow: hidden;
+  }
 `
 
 export default Animation
