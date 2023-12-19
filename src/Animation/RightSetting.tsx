53d6a23e1e22d51bab4c6e7c44678b7422a0e18d
zeroter <510432304@qq.com>
2023-12-19 11:11:01 +0800

    添加位置控制

--- src/Animation/RightSetting.tsx
+++ src/Animation/RightSetting.tsx
@@ -6,6 +6,8 @@ interface RightSettingProps {
   interval: number
   setState: (state: string) => void
   setInterval: (interval: number) => void
+  position: any
+  setPosition: (position: any) => void
 }
 const RightSetting: React.FC<RightSettingProps> = (props) => {
   const items = [
@@ -46,15 +48,65 @@ const RightSetting: React.FC<RightSettingProps> = (props) => {
           +0.25
         </button>
       </div>
+      <div className="position">
+        <div
+          onClick={() =>
+            props.setPosition((position: any) => {
+              return Object.assign({}, position, { bottom: position.bottom + 5 })
+            })
+          }
+          className="item"
+        >
+          ↑
+        </div>
+        <div className="position-lr">
+          <div
+            onClick={() =>
+              props.setPosition((position: any) => {
+                return Object.assign({}, position, { left: position.left - 5 })
+              })
+            }
+            className="item"
+          >
+            ←
+          </div>
+          <div
+            onClick={() =>
+              props.setPosition((position: any) => {
+                return Object.assign({}, position, { left: position.left + 5 })
+              })
+            }
+            className="item"
+          >
+            →
+          </div>
+        </div>
+        <div
+          onClick={() =>
+            props.setPosition((position: any) => {
+              return Object.assign({}, position, { bottom: position.bottom - 5 })
+            })
+          }
+          className="item"
+        >
+          ↓
+        </div>
+      </div>
+      <div style={{ fontSize: 12 }} className="position-value">
+        left: {props.position.left}
+        <br />
+        bottom: {props.position.bottom}
+      </div>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
   position: fixed;
-  width: 128px;
-  height: 150px;
+  min-width: 128px;
+  min-height: 150px;
   border-radius: 8px;
+  z-index: 9999;
 
   box-shadow: 0 0 10px #aaa;
   right: 10px;
@@ -66,6 +118,7 @@ const Wrapper = styled.div`
   justify-content: center;
   flex-direction: column;
   gap: 8px;
+  padding: 16px 8px;
 
   .item {
     width: 100px;
@@ -111,6 +164,28 @@ const Wrapper = styled.div`
         background-color: #ccc;
       }
     }
+  }
+
+  .position {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 8px;
+
+    .item {
+      width: 50px;
+      height: 20px;
+      cursor: pointer;
+      background-color: #eee;
+    }
+  }
+
+  .position-lr {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
   }
 `
 
--- src/Animation/index.tsx
+++ src/Animation/index.tsx
@@ -1,24 +1,35 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useState } from 'react'
 import RightSetting from './RightSetting'
 import Entity, { animationType } from './Entity'
 
 const Animation = () => {
   const [state, setState] = useState<string>('slide')
   const [interval, setInterval] = useState<number>(1)
-  const [isSmall, setIsSmall] = useState<boolean>(false)
+  // const [isSmall, setIsSmall] = useState<boolean>(false)
+  const [position, setPosition] = useState<{ left: number; bottom: number }>({
+    left: 20,
+    bottom: 100
+  })
 
-  useEffect(() => {
-    function updateClient() {
-      setIsSmall(window.innerWidth < 576)
-    }
+  // useEffect(() => {
+  //   function updateClient() {
+  //     setIsSmall(window.innerWidth < 576)
+  //   }
 
-    window.addEventListener('resize', updateClient)
-  }, [])
+  //   window.addEventListener('resize', updateClient)
+  // }, [])
 
   return (
     <div>
       <Entity state={state as animationType} interval={interval}></Entity>
-      <RightSetting setState={setState} state={state} interval={interval} setInterval={setInterval}></RightSetting>
+      <RightSetting
+        position={position}
+        setPosition={setPosition}
+        setState={setState}
+        state={state}
+        interval={interval}
+        setInterval={setInterval}
+      ></RightSetting>
     </div>
   )
 }
